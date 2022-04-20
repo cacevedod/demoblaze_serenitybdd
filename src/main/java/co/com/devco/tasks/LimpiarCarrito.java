62132685de73ac37c333d808d2964e3f4baab246
Carlos Acevedo <cacevedod@devco.com.co>
2022-04-20 11:36:17 -0500

    limpiar el carrito funcionando

--- src/main/java/co/com/devco/tasks/LimpiarCarrito.java
+++ src/main/java/co/com/devco/tasks/LimpiarCarrito.java
@@ -1,12 +1,12 @@
 package co.com.devco.tasks;
 
-import co.com.devco.interactions.ClickVariosElementos;
+import co.com.devco.interactions.ClickPrimerDeleteCarrito;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 
-import static co.com.devco.userinterfaces.DemoblazeCarritoPage.LINK_DELETE_PRODUCTOS_CARRITO;
+import static co.com.devco.userinterfaces.DemoblazeCarritoPage.*;
 import static co.com.devco.userinterfaces.DemoblazeIndexPage.LINK_CARRITO;
 import static co.com.devco.userinterfaces.DemoblazeIndexPage.LINK_HOME;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
@@ -21,7 +21,7 @@ public class LimpiarCarrito implements Task {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Click.on(LINK_CARRITO),
-                ClickVariosElementos.on(LINK_DELETE_PRODUCTOS_CARRITO),
+                ClickPrimerDeleteCarrito.elementos(PRODUCTOS_CARRITO),
                 Click.on(LINK_HOME)
         );
     }
